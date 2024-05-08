8e62f824f5747f765946245e7a699bbf2fde311b
fchau1 <fchau@nevada.unr.edu>
2024-05-07 20:36:06 -0700

    Added Test Folder

---
+++ tests/test_IntegrationTestInsertData.py
@@ -0,0 +1,26 @@
+import unittest
+from DatabaseManager import DatabaseManager
+from Database import Database
+from Table import Table
+
+class IntegrationTestInsertData(unittest.TestCase):
+    def setUp(self):
+        self.table = Table(["name", "year"])
+        self.database = Database()
+        self.database.add_table(self.table, "Students") 
+
+        self.manager = DatabaseManager()
+        self.manager.add_database(self.database, "School")  
+        self.manager.use_database("School")
+
+    def test_insert_data(self):
+        self.manager.current_database.tables["Students"].insert(["Anna", 1])
+        names = self.manager.current_database.tables["Students"].data["name"]
+        years = self.manager.current_database.tables["Students"].data["year"]
+
+        self.assertEqual(self.table.data["name"], ["Anna"])
+        self.assertEqual(self.table.data["year"], [1])
+
+if __name__ == "__main__":
+    unittest.main()
+
